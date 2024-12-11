3fe37700b8e1d6610b0f8147d815d04cb505ac7c
mitul88 <shahed.rahman88@gmail.com>
2024-12-11 21:21:59 +0600

    redis service added

---
+++ src/service/redis.service.ts
@@ -0,0 +1,31 @@
+import { createClient } from "redis";
+
+const client = createClient();
+
+client.on("error", (err: Error) => console.log("Redis is not connected!"));
+
+client.connect().then(() => console.log("redis connected!"));
+
+export const setValue = async (key: string, value: string) => {
+  return client.set(key, value);
+};
+
+export const getValue = async (key: string, value: string) => {
+  return client.get(key);
+};
+
+export const getObjValue = async (key: string) => {
+  return client.hGetAll(key);
+};
+
+export const setObjValue = async (key: string, value: { string: string }) => {
+  return client.hSet(key, value);
+};
+
+export const setObjPropValue = async (
+  key: string,
+  prop: string,
+  val: string
+) => {
+  return client.hSet(key, prop, val);
+};
